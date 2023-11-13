b19f12a1f662c2583289c69f1940b03157e156f3
Renato Palominos <rpalominos@utem.cl>
2023-11-13 19:28:13 -0300

    Clean code

--- App.tsx
+++ App.tsx
@@ -1,5 +1,4 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
 import HomeView from './src/views/homeView';
 
 export default function App() {
--- src/views/homeView.tsx
+++ src/views/homeView.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
+import { View } from 'react-native';
 import Body from '../containers/Body';
 
 
